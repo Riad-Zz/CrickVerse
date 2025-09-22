8487343defca59f8d40fca189da8f0e9caab665f
Riad <kazuha3242@gmail.com>
2025-09-23 02:08:09 +0600

    feat : Navigation section added with responsiveness

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Logo from '../../assets/logo.png'
+import Coin from '../../assets/coin.png'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar max-w-11/12 mx-auto mt-2 mb-6">
+                <div className="flex-1">
+                    <img src={Logo} alt="Crickverse Logo" className='h-12 w-12 md:h-auto w-auto' />
+                </div>
+                <div>
+                    <div className='flex gap-12 items-center'>
+                        <div className='gap-12 items-center hidden md:flex'>
+                            <div className=' text-[#131313b3]'>
+                                <a href="#">Home</a>
+                            </div>
+                            <div className=' text-[#131313b3]'>
+                                <a href="#">Fixture</a>
+                            </div>
+                            <div className=' text-[#131313b3]'>
+                                <a href="#">Teams</a>
+                            </div>
+                            <div className=' text-[#131313b3]'>
+                                <a href="#">Schedules</a>
+                            </div>
+                        </div>
+
+
+                        <button className='btn px-5 py-4 bg-white rounded-xl'>
+                            <div className='font-bold'>
+                                <span className='mr-1'>0</span>Coin
+                            </div>
+                            <img src={Coin} alt="" className='w-6 h-6' />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
