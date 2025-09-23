29c3608c3e53cdf56ef67b810cf6a4f95451fee5
Riad <kazuha3242@gmail.com>
2025-09-23 23:49:10 +0600

    Preparing for git deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = 'CrickVerse'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
 })
